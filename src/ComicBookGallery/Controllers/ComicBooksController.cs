a3af064492c6e7a47ac20d8b80cb05acfa85f615
KareemDev1984 <karim.lamrini10@gmail.com>
2019-03-07 22:18:27 +0100

    Adding an ActionMethod to the controller

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -6,7 +6,15 @@ using System.Web.Mvc;
 
 namespace ComicBookGallery.Controllers
 {
-    public class ComicBooksController : Controller //the comocbookscontroller class we added inherits from controller class (using system.web.mvc)
+    //the comicbookscontroller class we added inherits from controller class (using system.web.mvc)
+    //controllerclasses need to be public otherwise MVC won't be able to find and use them.
+    public class ComicBooksController : Controller
     {
+        //ActionMethods need to be public too!!
+        public string Detail()
+        {
+            return $"Hello from the comic books controller";
+        }
+
     }
 }
