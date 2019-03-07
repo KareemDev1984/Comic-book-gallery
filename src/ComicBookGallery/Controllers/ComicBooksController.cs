bb9a99249bfb8bad63255292ca8ec4f8e0b25c2f
KareemDev1984 <karim.lamrini10@gmail.com>
2019-03-07 22:50:21 +0100

    Updated actionMethod

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -11,9 +11,23 @@ namespace ComicBookGallery.Controllers
     public class ComicBooksController : Controller
     {
         //ActionMethods need to be public too!!
-        public string Detail()
+        //contentresult and redirectresult havce a common baseClass ==> ActionResult base class
+        //because of this ActionResult is almost always used as the returnType
+        public ActionResult Detail() 
         {
-            return $"Hello from the comic books controller";
+            if (DateTime.Today.DayOfWeek == DayOfWeek.Thursday)
+            {
+                //return new RedirectResult("");
+                return Redirect("/"); //we can call the redirect method that is part of our controllerclass
+            }
+            return Content("Hello from the comic books controller");
+
+
+            // not neccesary! we can directly call the Content() method that is part of the controllerclass
+            //return new ContentResult()
+            //{ Content= "Hello from the comic books controller" };
+
+          
         }
 
     }
