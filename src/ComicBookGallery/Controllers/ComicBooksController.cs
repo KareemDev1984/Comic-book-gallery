81cfacdf1834fcd5c392f9b007d9d5937684d62b
karim.lamrini10 <karim.lamrini10@gmail.com>
2019-03-08 13:21:51 +0100

    Added Comic Book Detail view

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+
 namespace ComicBookGallery.Controllers
 {
     //the comicbookscontroller class we added inherits from controller class (using system.web.mvc)
@@ -15,19 +16,22 @@ namespace ComicBookGallery.Controllers
         //because of this ActionResult is almost always used as the returnType
         public ActionResult Detail() 
         {
-            if (DateTime.Today.DayOfWeek == DayOfWeek.Thursday)
-            {
-                //return new RedirectResult("");
-                return Redirect("/"); //we can call the redirect method that is part of our controllerclass
-            }
-            return Content("Hello from the comic books controller");
+            //if (DateTime.Today.DayOfWeek == DayOfWeek.Thursday)
+            //{
+            //    //return new RedirectResult("");
+            //    return Redirect("/"); //we can call the redirect method that is part of our controllerclass
+            //}
+            //return Content("Hello from the comic books controller");
 
 
             // not neccesary! we can directly call the Content() method that is part of the controllerclass
             //return new ContentResult()
             //{ Content= "Hello from the comic books controller" };
 
-          
+            //return new ViewResult();
+            return View();
+
+
         }
 
     }
